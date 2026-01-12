0dc9ceffa0027ec43e18de5a60140a78c1d362bc
Lukasz Kowalski <lukasz.kowalski@tailoredapps.pl>
2026-01-12 23:59:09 +0100

    webmail

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <div>Teletext Mode Enabled · Use arrow keys to browse · Press 0 to return to page 100 · Contact: redakcja@3ktv.pl</div>
+      <div>Teletext Mode Enabled · Use arrow keys to browse · Press 0 to return to page 100 · Contact: redakcja@3ktv.pl
+        <a href='/webmail'>Webmail</a>
+      </div>
     </footer>
   );
 };
