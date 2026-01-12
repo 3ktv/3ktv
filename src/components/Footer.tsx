809b4fce1dd17aad11324e2b3e3f543bc3e271ea
Lukasz Kowalski <lukasz.kowalski@tailoredapps.pl>
2026-01-13 00:03:07 +0100

    fix button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div>Teletext Mode Enabled · Use arrow keys to browse · Press 0 to return to page 100 · Contact: redakcja@3ktv.pl
-        <a href='/webmail'>Webmail</a>
+         <a href='#webmail'>Webmail</a>
       </div>
     </footer>
   );
